193b800aacfbb06dfc25704e5c8b77f633a16114
Aruzhan <66684270+aruzhansadakbayeva@users.noreply.github.com>
2020-12-07 17:53:31 +0600

    Add files via upload

---
+++ endterm_ordered/1.py
@@ -0,0 +1,14 @@
+
+def c(n, x): 
+  
+    ans = 0; 
+    for i in range(1, n+1 ): 
+        if (x % i == 0 and x / i <= n): 
+            ans += 1
+  
+    print(ans)
+
+
+n=int(input())
+x=int(input())
+c(n,x)
